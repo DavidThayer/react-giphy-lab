01e222fa5e026b297d8112abf4044843a4bdb60b
kbbushman <k.bushman@yahoo.com>
2018-05-03 10:58:01 -0700

    Fixes heroku mixed active content error

--- src/models/GiphyModel.js
+++ src/models/GiphyModel.js
@@ -4,13 +4,13 @@ const API_KEY = 'g7FJSuBOaK5Be2W7dM6NceeBQ29JmuoG';
 class GiphyModel {
   static search = (query) => {
     
-    const request = axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`);
+    const request = axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`);
     return request;
   }
 
   static searchById = (id) => {
     
-    const request = axios.get(`http://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`);
+    const request = axios.get(`https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`);
     return request;
   }
 }
